e733581424d0c9b99094c5c044d27b0aa3625c62
Pooja Diwakar <poojadiwakar@gmail.com>
2016-07-09 16:04:19 -0700

    Dream job title and wages
    Asking for Dream job title and wages

---
+++ Practice/FirstHomeWork.py
@@ -0,0 +1,9 @@
+'''Asking user to enter their Dream Job Title'''
+
+dream_job_title = raw_input("Enter your Dream Job Title : ")
+print "Wow! You want to become a",dream_job_title
+hourly_wage = raw_input("How much hourly wage you want to earn? $")
+wage = double(hourly_wage)
+
+
+
